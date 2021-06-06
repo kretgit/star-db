23739475d35ce7bedb297793b166cd642b1a4c8e
kretgit <kret86@yandex.ru>
2021-06-06 18:22:19 +0300

    changes...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 
 
-console.log("hello!!");
+const getResource = async (url) => {
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`fetch error ${url} result is: ${res.status}`)
+    }
+    return await res.json();
+};
+
+getResource('https://swapi.dev/api/people/1444444444/')
+    .then((body) => {
+        console.log(body)
+    })
+    .catch(err => {
+       console.error("got error :" + err)
+    });
+
+// fetch('https://swapi.dev/api/people/1/')
+//     .then((data) => {
+//         return data.json();
+//     })
+//     .then((body) => {
+//         console.log(body)
+//     });
